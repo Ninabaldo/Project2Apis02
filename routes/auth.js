df72a6a564f67ac2b06c1ba9e8eec7fa1f6ef6bb
Ninabaldo <nbaldorousseau@gmail.com>
2021-09-17 11:06:36 +0200

    new changes

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,8 @@
 const router = require("express").Router();
-
-// ℹ️ Handles password encryption
 const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
-
-// How many rounds should bcrypt run the salt (default [10 - 12 rounds])
 const saltRounds = 10;
-
-// Require the User model in order to interact with the database
 const User = require("../models/User.model");
-
-// Require necessary (isLoggedOut and isLiggedIn) middleware in order to control access to specific routes
 const isLoggedOut = require("../middleware/isLoggedOut");
 const isLoggedIn = require("../middleware/isLoggedIn");
 
--- routes/profile.js
+++ routes/profile.js
@@ -3,7 +3,9 @@ const router = require("express").Router();
 const Color= require("../models/Color.model");
 const User= require("../models/User.model");
 const isLoggedIn= require("../middleware/isLoggedIn");
-/* GET profile page */
+
+
+/* ADD TO FAVOURITE IN YOUR PROFILE PAGE */
 router.post("/add-favourite", isLoggedIn, (req, res, next) => { 
   const {color,img}= req.body
   Color.create({color:color.toString(),img})
@@ -19,5 +21,21 @@ router.post("/add-favourite", isLoggedIn, (req, res, next) => {
   })
 
   });
+
+/* DELATE FAVOURITE */
+  router.post("/delete-favourite",isLoggedIn,(req,res,next)=>{
+    const {color,img}= req.body
+    Color.delate({color:color.toString(),img})
+    .then((color)=>{
+      User
+     .findByIdAndUpdate(req.user._id,{$pull : {favourites :color._id}})
+     .then(()=>{
+      res.redirect("/auth/profile")
+     })
+     .catch(err => 
+      console.log(err))
+    })
+   
+})
   
   module.exports = router;
