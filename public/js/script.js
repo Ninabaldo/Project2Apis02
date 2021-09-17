953be1dbbbb174229209d5d847236588fc8e0890
Ninabaldo <nbaldorousseau@gmail.com>
2021-09-17 17:21:21 +0200

    more visual changes (picker)

--- public/js/script.js
+++ public/js/script.js
@@ -5,3 +5,27 @@ document.addEventListener(
   },
   false
 );
+
+// Inputs
+const valueInput = document.querySelector('input[type="text"]');
+const colorInput = document.querySelector('input[type="color"]');
+
+// Sync the color from the picker
+const syncColorFromPicker = () => {
+  valueInput.value = colorInput.value;
+};
+
+// Sync the color from the field
+const syncColorFromText = () => {
+  colorInput.value = valueInput.value;
+};
+
+// Bind events to callbacks
+colorInput.addEventListener("input", syncColorFromPicker, false);
+valueInput.addEventListener("input", syncColorFromText, false);
+
+// Optional: Trigger the picker when the text field is focused
+valueInput.addEventListener("focus", () => colorInput.click(), false);
+
+// Refresh the text field
+syncColorFromPicker();
